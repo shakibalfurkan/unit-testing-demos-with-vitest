0b4824701db4996b2f9067a9b8c78ffaaf7ca357
shakibalfurkan <shakibalfurkan@gmail.com>
2025-10-15 17:31:14 +0600

    toStrictEqual and toBeTruthy matcher explored

--- test/matcher.test.js
+++ test/matcher.test.js
@@ -15,3 +15,19 @@ it("toEqual matcher", () => {
 
   expect(obj1).toEqual(obj2);
 });
+
+// toStrictEqual: check equality deeply just like toEqual(), but also consider properties like undefined, function and other types that are not strict in certain cases.
+
+it("toStrictEqual", () => {
+  const obj1 = { name: "shakib", age: 19 };
+  const obj2 = { name: "shakib", age: 19 };
+
+  expect(obj1).toStrictEqual(obj2);
+});
+
+// toBeTruthy: checks is the value is true or not
+
+it("toBeTruthy matcher", () => {
+  const value = 1;
+  expect(value).toBeTruthy();
+});
